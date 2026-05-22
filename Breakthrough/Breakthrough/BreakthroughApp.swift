e81c1c75af8930959cda910121532a72811deae9
Alexander Nusov <nusov@users.noreply.github.com>
2026-05-22 19:52:21 +0300

    add empty project (swift 6)

---
+++ Breakthrough/Breakthrough/BreakthroughApp.swift
@@ -0,0 +1,32 @@
+//
+//  BreakthroughApp.swift
+//  Breakthrough
+//
+//  Created by Alexander Nusov on 5/22/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BreakthroughApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
